a7ee615db8cb678042d42c23fb5709572eaba60c
AtwongereVianney <atwongerevianney@gmail.com>
2025-08-11 12:01:18 +0300

    Edit Migration Files

---
+++ database/migrations/2025_08_11_080211_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+{
+    Schema::create('bookings', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('student_id')->constrained()->onDelete('cascade');
+        $table->foreignId('room_id')->constrained()->onDelete('cascade');
+        $table->date('start_date');
+        $table->date('end_date')->nullable();
+        $table->enum('status', ['active', 'completed', 'cancelled'])->default('active');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
