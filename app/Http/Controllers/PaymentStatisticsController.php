8bdea7de7a4ccea31b1d2e38f2177d5542fe3138
AtwongereVianney <atwongerevianney@gmail.com>
2025-08-26 20:14:55 +0300

    edit PaymentStatisticsController.php

--- app/Http/Controllers/PaymentStatisticsController.php
+++ app/Http/Controllers/PaymentStatisticsController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class PaymentStatisticsController extends Controller
 {
-    //
+    public function index()
+    {
+        // Fetch payment statistics data from the database
+        $paymentStatistics = []; // Replace with actual data fetching logic
+
+        return view('paymentStatistics.index', compact('paymentStatistics'));
+    }
 }
